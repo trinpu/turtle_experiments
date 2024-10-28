e6f14f97fe417683099443fd056b02ad8989e9b3
Pierre <pierre@pierrecollura.com>
2024-10-28 16:28:25 +0000

    Isolate features related to simple spiral drawing

--- basic_behaviour/sizing.py
+++ basic_behaviour/sizing.py
@@ -17,45 +17,9 @@ def draw_spiral(a_turtle, pen_size=1, pen_size_change=0, edge_size = 20, edge_si
 
     return 0
 
-
-def experiment_spiral(a_turtle, pen_size=1, pen_size_change=0, edge_size = 20, edge_size_change = 5):
-
-    n_edges = 4 * 1 # 1 is for single spiral
-    angle_remeinder = 89 * n_edges
-    
-    edge_remeinder = sum([x for x in range(n_edges)]) # must figure out the logic. [10, 15, 20, 25] == 70 == (10*4) + 30
-
-    angle_changes = []
-    edges_sum = []
-
-    for edge in range(n_edges):
-        if edge < 3:  
-            angle_size = random.randint(0, 89)
-            edge_size = random.randint(0, edge_size + edge_size_change)
-
-            a_turtle.forward(edge_size)
-            a_turtle.left(angle_size)
-
-            angle_remeinder -= angle_size
-            edge_remeinder -= edge_size
-        else:
-            angle_size = angle_remeinder
-            edge_size = edge_remeinder
-
-            a_turtle.forward(edge_size)
-            a_turtle.left(angle_size)
-
-    return 0
-
 charlie = turtle.Turtle()
 charlie.color("green")
 draw_spiral(charlie)
-
-frank = turtle.Turtle()
-frank.color("yellow")
-experiment_spiral(frank)
-
-    
 
 canvas.mainloop() 
 

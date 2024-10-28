ae322bc73c1c96e9bb3fd102d2676048b9b23a84
Pierre <pierre@pierrecollura.com>
2024-10-28 09:53:25 +0000

    Abstract spiral drawing and randomize angle change

--- basic_behaviour/sizing.py
+++ basic_behaviour/sizing.py
@@ -1,6 +1,7 @@
 # ref https://docs.python.org/3/library/turtle.html
 
 import turtle
+import random
 
 canvas = turtle.Screen()
 canvas.bgcolor("black")
@@ -8,17 +9,19 @@ canvas.bgcolor("black")
 the_turtle = turtle.Turtle()
 the_turtle.color("green")
 
-pen_size = 1
-pen_size_change = 1
-edge_size = 50
-edge_size_change = 10
+def draw_spiral(pen_size=1, pen_size_change=0, edge_size = 20, edge_size_change = 10):
+    for edge in range(20):
+        angle_change = random.randint(89,105) # 89 is the usual constant
+        the_turtle.pensize(pen_size)
+        the_turtle.forward(edge_size)
+        the_turtle.left(angle_change)
+        pen_size += pen_size_change
+        edge_size += edge_size_change
 
-for edge in range(50):
-    the_turtle.pensize(pen_size)
-    the_turtle.forward(edge_size)
-    the_turtle.left(89)
-    pen_size += pen_size_change
-    edge_size += edge_size_change
+    return 0
+
+draw_spiral()
+
 
     
 

c7d98244dcd4e44661728b107a783888b9898655
Pierre Collura <66747274+trinpu@users.noreply.github.com>
2024-11-02 23:15:40 +0000

    Add the coloring logic for the background and hour hands

--- lost_in_london/clock.py
+++ lost_in_london/clock.py
@@ -1,8 +1,12 @@
 import turtle
 
+background_color = "#090c1f"
+pen_color = "#BFEDFF"
+fill_color = "#6490CE"
 
 canvas = turtle.Screen()
-canvas.bgcolor("#090c1f")
+
+canvas.bgcolor(background_color)
 
 n_hour_marks = 12
 hour_mark_angle = 360 / n_hour_marks
@@ -10,12 +14,13 @@ hour_hand_size = 200
 hour_mark_heading = 0
 hour_marks = []
 
+
 for mark in range(n_hour_marks):
     a_turtle = turtle.Turtle()
     a_turtle.shape('circle')
     a_turtle.setheading(hour_mark_heading)
     a_turtle.shapesize(1)
-    a_turtle.color("#EBD615", "#EBA215")
+    a_turtle.color(pen_color, background_color)
     a_turtle.penup()
     a_turtle.forward(hour_hand_size)
     hour_mark_heading += hour_mark_angle

08eadf0ebd391c37916299580770900ba7884ff5
Pierre Collura <66747274+trinpu@users.noreply.github.com>
2024-11-02 23:17:05 +0000

    Introduce clockwise hand creation, with correct clock order

--- lost_in_london/clock.py
+++ lost_in_london/clock.py
@@ -11,7 +11,7 @@ canvas.bgcolor(background_color)
 n_hour_marks = 12
 hour_mark_angle = 360 / n_hour_marks
 hour_hand_size = 200
-hour_mark_heading = 0
+hour_mark_heading = 90
 hour_marks = []
 
 
@@ -23,7 +23,7 @@ for mark in range(n_hour_marks):
     a_turtle.color(pen_color, background_color)
     a_turtle.penup()
     a_turtle.forward(hour_hand_size)
-    hour_mark_heading += hour_mark_angle
+    hour_mark_heading -= hour_mark_angle
     hour_marks.append(a_turtle)
 
 canvas.mainloop()
